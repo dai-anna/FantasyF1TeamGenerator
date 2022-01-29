7b37008fbb752ff739a42c06c8b5ef6813ced6e7
EC2 Default User <ec2-user@ip-172-31-25-45.ec2.internal>
2022-01-29 19:38:18 +0000

    add

--- backend.py
+++ backend.py
@@ -5,19 +5,31 @@ import pandas as pd
 
 app = FastAPI()
 
-years = 2020
-rand_driver_idx = 0
-
+rand_driver_idx=0
 
 @app.get("/")
 def root():
-    r = requests.get(f"http://ergast.com/api/f1/{years}/driverStandings.json")
+    r = requests.get(f"http://ergast.com/api/f1/current/driverStandings.json")
     data = r.json()["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"][
         rand_driver_idx
     ]["Driver"]
     # ["Driver"]  # []["givenName"]
-    return data
+    output = "The current year's winner is number: {0}, {1} {2} ({3})! For other years, input year (between 2005-2021) at the end of the URL (e.g. /2020)".format(data["permanentNumber"], data["givenName"], data["familyName"],data["code"])
+    return output
 
+
+@app.get("/{year}")
+def years(year):
+    if int(year) >= 2005:
+        r = requests.get(f"http://ergast.com/api/f1/{year}/driverStandings.json")
+        data = r.json()["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"][
+            rand_driver_idx
+        ]["Driver"]
+        # ["Driver"]  # []["givenName"]
+        output = "The winner in {4} is number: {0}, {1} {2} ({3})!".format(data["permanentNumber"], data["givenName"], data["familyName"],data["code"], year)
+    else:
+        output = "Uhh... sorry we don't have this data :("
+    return output
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8080, host="0.0.0.0")
